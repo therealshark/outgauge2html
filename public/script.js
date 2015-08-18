b23b1dc8e4093e246d10617048df0f87d00c6816
theshark <mweigel@beamng.com>
2015-08-18 14:26:28 +0200

    improved rendering

--- public/script.js
+++ public/script.js
@@ -2,17 +2,40 @@
 // Socket.io init and updating data
 var socket = io.connect();
 var outgaugeData;
+var gauges = [];
+var updateAvailable = false;
+var renderStarted = false;
 
+// update
 socket.on('outgauge',function(data){
     // TODO: Quicky slapped together, this needs quite some work
     data.kph = data.speed * 3.6;
     outgaugeData = data;
-    var formatted = JSON.stringify(data,null,'  ').replace(/\n/g,'<br>').replace(/  /g,'&nbsp;&nbsp;&nbsp;&nbsp;');//
-    document.getElementById('msg').innerHTML = formatted;
+    updateAvailable = true;
+    if(!renderStarted){
+        render();
+        renderStarted = true;
+    }
 });
 
+// rendercycle
+function render(){
+    if(updateAvailable){
+        // update debug
+        document.getElementById('msg').innerHTML = JSON.stringify(outgaugeData,null,'  ')
+            .replace(/\n/g,'<br>')
+            .replace(/  /g,'&nbsp;&nbsp;&nbsp;&nbsp;');
+        // update gauges
+        gauges.forEach(function(gauge){
+            gauge.gadget.setValue(outgaugeData[gauge.field]);
+        });
+        updateAvailable = false;
+    }
+    requestAnimationFrame(render);
+}
+
 // Generate and update gauges
-var gauges = [
+var gaugeConfig = [
     {
         field: 'rpm',
         size: 300,
@@ -45,7 +68,7 @@ var gauges = [
     }
 ];
 
-gauges.forEach(function(gauge){
+gaugeConfig.forEach(function(gauge){
     console.log(gauge);
     var g = new Gauge({
         renderTo: gauge.field,
@@ -63,12 +86,8 @@ gauges.forEach(function(gauge){
             needle     : { start : 'rgba(240, 128, 128, 1)', end : 'rgba(255, 160, 122, .9)' }
         }
     });
-    var update = function(){
-        if(outgaugeData){
-            g.setValue(outgaugeData[gauge.field]);
-        }
-        requestAnimationFrame(update);
+    g.onready = function(){
+        gauges.push({field: gauge.field, gadget: g});
     };
-    g.onready = update;
     g.draw();
 });
