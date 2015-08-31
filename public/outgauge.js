933dcb405bac71ac006e1f528c842d67f4bac8e4
theshark <mweigel@beamng.com>
2015-08-31 16:58:08 +0200

    implemented constants, implemented enriched lightsobjects

--- public/outgauge.js
+++ public/outgauge.js
@@ -28,12 +28,55 @@ window.OutGauge = (function(){
     }
 
     function OutGaugeWrapper(d){
+        // bit constants
+        var OG = {
+            OG_SHIFT: 1,
+            OG_CTRL: 2,
+
+            OG_TURBO: 8192,
+            OG_KM: 16384,
+            OG_BAR: 32786
+        };
+        var DL = {
+            SHIFT: 1,
+            FULLBEAM: 2,
+            HANDBRAKE: 4,
+            PITSPEED: 8,
+            TC: 16,
+            SIGNAL_L: 32,
+            SIGNAL_R: 64,
+            SIGNAL_ANY: 128,
+            OILWARN: 256,
+            BATTERY: 512,
+            ABS: 1024,
+            SPARE: 2048,
+            NUM: 4096
+        };
+
         var data = d;
         var self = this;
+
         // enrich data
+        // speed
         data.speedKPH = data.speed * 3.6;
         data.speedMPH = data.speed * 2.23694;
+        // gear
         data.realGear = data.gear - 1;
+        // lights
+        var enrichedFields = {
+            dashlights: 'lightsPresent',
+            showlights: 'lightsActive'
+        };
+        Object.keys(enrichedFields).forEach(function(field){
+            var enrichedField = enrichedFields[field];
+            data[enrichedField] = [];
+            Object.keys(DL).forEach(function(key){
+                if(data[field] & DL[key]){
+                    data[enrichedField].push(key);
+                }
+            });
+        });
+
 
 
         Object.keys(data).forEach(function(key){
